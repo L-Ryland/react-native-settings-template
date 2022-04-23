d0d4c872300596cb6172dcbd33f13230a7d9208e
L-Ryland <saintleso@gmail.com>
2022-04-23 18:07:56 +0800

    test

--- src/Chevron.tsx
+++ src/Chevron.tsx
@@ -1,19 +1,22 @@
-import * as React from "react";
-// import {  StyleSheet } from "react-native";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React from "react";
+import {View, StyleSheet} from "react-native"
+// import Entypo from 'react-native-vector-icons/Entypo';
+import { Entypo } from "@expo/vector-icons";
 
 export const Chevron = ():JSX.Element => {
   return (
-      <Icon name="chevron-right" size={24} color="black" />
+    <View style={defaultStyles.chevron}>
+      <Entypo name="chevron-right" size={24} color="black" />
+    </View>
   );
 };
 
-// const defaultStyles = StyleSheet.create({
-//   chevron: {
-//     width: 12,
-//     height: 12,
-//     marginLeft: 8,
-//     marginRight: 10,
-//     opacity: 0.35,
-//   },
-// });
+const defaultStyles = StyleSheet.create({
+  chevron: {
+    width: 12,
+    height: 12,
+    marginLeft: 8,
+    marginRight: 10,
+    opacity: 0.35,
+  },
+});
